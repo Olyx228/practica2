b45077b5a14cf97f49e821b5c95ee91d3866fa4d
Семён Нефедов <gr424_nesal@lab-402-8.tomtit-tomsk.ru>
2024-11-01 11:49:23 +0700

    HTMLCategoryAndAdmins

--- user/admin.py
+++ user/admin.py
@@ -9,5 +9,5 @@ class RequestAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name']
-    search_fields = ['name']
+    list_display = ('id', 'name')
+    search_fields = ('name',)
