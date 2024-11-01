7020bfee13e77e1e27e199bb1a765297edda3e2b
Семён Нефедов <gr424_nesal@lab-402-8.tomtit-tomsk.ru>
2024-11-01 13:27:02 +0700

    AdminPhotoComment

--- user/admin.py
+++ user/admin.py
@@ -1,23 +1,31 @@
 from django.contrib import admin
 from .models import Request, Category
 
+
 @admin.register(Request)
 class RequestAdmin(admin.ModelAdmin):
     list_display = ['title', 'status', 'created_at', 'user']
     list_filter = ['status', 'created_at']
-    readonly_fields = ('title', 'description', 'category', 'user')
     search_fields = ['title', 'description', 'user__username']
+    readonly_fields = ('title', 'description', 'category', 'user')
 
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
-        if obj is not None:  # Проверяем, что объект существует
-            print(f"Статус заявки: {obj.status}")  # Выводим статус в консоль
-            if obj.status == 'new':  # Проверяем статус
-                form.base_fields['comment'].disabled = True  # Делаем поле комментариев недоступным
+
+        if obj is not None:
+
+            if obj.status == 'new':
+                form.base_fields['comment'].disabled = True
+
+            if obj.status == 'completed':
+                form.base_fields['photo'].required = True
+            else:
+                form.base_fields['photo'].required = False
+
         return form
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
     search_fields = ['name']
-
